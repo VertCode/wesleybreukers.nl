2553aa6a3476ff4dacb33e087c6cb898b7ad2f9a
VertCode <wesleybreukers2004@hotmail.com>
2021-04-15 10:40:21 +0200

    Finished contact.php (Contact Me Page)

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include("./parts/header.php")
 ?>
-    <link rel="stylesheet" href="../src/css/home.css">
+    <link rel="stylesheet" href="./src/css/home.css">
     <div class="welcome-message">
         <div class="vertical-line"></div>
         <h2>"Welcome to my</h2>
--- pages/about-me.php
+++ pages/about-me.php
@@ -1,6 +1,7 @@
 <?php
 include("../parts/header.php")
 ?>
+    <link rel="stylesheet" type="text/css" href="../src/css/about-me.css">
 
 <?php
 include("../parts/footer.php")
